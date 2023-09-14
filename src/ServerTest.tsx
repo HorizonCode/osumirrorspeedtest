38262763e3f87971317235b44ac0a0c5f66568cf
HorizonCode <horizoncode88@gmail.com>
2023-09-14 21:21:05 +0200

    add rps, fix latency calculation executing multiple times

--- src/ServerTest.tsx
+++ src/ServerTest.tsx
@@ -1,43 +1,65 @@
 import { useEffect, useState } from "react";
 import "./ServerTest.scss";
-import noLogo from './assets/nologo.webp';
+import noLogo from "./assets/nologo.webp";
 import { calculateAverageWithMinMax } from "./mathUtil";
-import prettytime from 'pretty-time';
+import prettytime from "pretty-time";
 
 function ServerTest(props: Record<string, string | number>) {
   const [imgSrc, setImgSrc] = useState(props["logo"]);
+
+  let calculating = false;
 
   const [averageSpeed, setAverageSpeed] = useState("calculating...");
   const [fastestRequest, setFastestRequest] = useState("calculating...");
   const [slowestRequest, setSlowestRequest] = useState("calculating...");
   const [failedRequests, setFailedRequests] = useState("calculating...");
+  const [requestsPerSecond, setRequestsPerSecond] = useState("calculating...");
 
   let maxRequests = props["requestAmount"] as number;
 
   const calculateLatency = async () => {
+    if (calculating) return;
+    calculating = true;
+    console.log("Start calculating " + props["serverName"]);
     const samples: number[] = [];
-    for(let sample = 0; sample < maxRequests; sample++){
-      const latencyStart = performance.now();
-      const request = await fetch(props["apiUrl"] as string);
-      if(!request.ok) continue;
-      const totalLatency = performance.now() - latencyStart;
-      samples.push(totalLatency);
-      setFailedRequests("?/" + samples.length);
-      await new Promise(res => setTimeout(res, 500));
-    }
+    const startTime = performance.now();
+    for (let sample = 0; sample < maxRequests; sample++) {
+      try {
+        const latencyStart = performance.now();
+        const request = await fetch(props["apiUrl"] as string);
+        if (!request.ok) throw new Error();
+        const totalLatency = performance.now() - latencyStart;
+        samples.push(totalLatency);
+        const result = calculateAverageWithMinMax(samples);
+        const average = prettytime([
+          Math.trunc(result.average / 1000),
+          Math.trunc(result.average * 1000000),
+        ]);
+        const highest = prettytime([
+          Math.trunc(result.highest / 1000),
+          Math.trunc(result.highest * 1000000),
+        ]);
+        const lowest = prettytime([
+          Math.trunc(result.lowest / 1000),
+          Math.trunc(result.lowest * 1000000),
+        ]);
+        setFastestRequest(lowest);
+        setSlowestRequest(highest);
+        setAverageSpeed(average);
+      } catch (err) {}
+      const droppedRequests = Math.abs(sample + 1 - samples.length);
+      setFailedRequests(droppedRequests + "/" + samples.length);
 
-    const result = calculateAverageWithMinMax(samples);
-    const average = prettytime([Math.trunc(result.average / 1000), Math.trunc(result.average * 1000000)]);
-    const highest = prettytime([Math.trunc(result.highest / 1000), Math.trunc(result.highest * 1000000)]);
-    const lowest = prettytime([Math.trunc(result.lowest / 1000), Math.trunc(result.lowest * 1000000)]);
-    setFastestRequest(lowest);
-    setSlowestRequest(highest);
-    setAverageSpeed(average);
+      const endTime = performance.now();
+      const elapsedSeconds = (endTime - startTime) / 1000;
+      const rps = samples.length / elapsedSeconds;
+      setRequestsPerSecond(rps.toFixed(2));
+      await new Promise((res) => setTimeout(res, 500));
+    }
 
     const droppedRequests = Math.abs(maxRequests - samples.length);
     setFailedRequests(droppedRequests + "/" + maxRequests);
-
-  }
+  };
 
   useEffect(() => {
     calculateLatency();
@@ -46,26 +68,39 @@ function ServerTest(props: Record<string, string | number>) {
   return (
     <>
       <div className="server-card">
-        <div className="server-icon"><img width="64" height="64" src={imgSrc as string} onError={() =>setImgSrc(noLogo)}></img></div>
-        <div className="server-info">
-          <div className="server-info-title">Server</div>
-          <a href={"https://" + props["serverName"]} target="_blank">{props["serverName"]}</a>
+        <div className="server-icon">
+          <img
+            width="64"
+            height="64"
+            src={imgSrc as string}
+            onError={() => setImgSrc(noLogo)}
+          ></img>
         </div>
         <div className="server-info">
-          <div className="server-info-title">Average Speed</div>
+          <div className="server-info-title">Server</div>
+          <a href={"https://" + props["serverName"]} target="_blank">
+            {props["serverName"]}
+          </a>
+        </div>
+        <div className="server-info">
+          <div className="server-info-title">Average speed</div>
           {averageSpeed}
         </div>
         <div className="server-info">
-          <div className="server-info-title">Fastest Request</div>
+          <div className="server-info-title">Fastest request</div>
           {fastestRequest}
         </div>
         <div className="server-info">
-          <div className="server-info-title">Slowest Request</div>
+          <div className="server-info-title">Slowest request</div>
           {slowestRequest}
         </div>
         <div className="server-info">
-          <div className="server-info-title">Failed Requests</div>
+          <div className="server-info-title">Failed requests</div>
           {failedRequests}
+        </div>
+        <div className="server-info">
+          <div className="server-info-title">Requests per second</div>
+          {requestsPerSecond}
         </div>
       </div>
     </>
