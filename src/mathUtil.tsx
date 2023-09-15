abd48cdea79b03b282e7a3564ead749bf7222ea5
HorizonCode <horizoncode88@gmail.com>
2023-09-15 15:47:17 +0200

    simplify calculateAverageWithMinMax

--- src/mathUtil.tsx
+++ src/mathUtil.tsx
@@ -1,20 +1,16 @@
-export function calculateAverageWithMinMax(numbers: number[]): { average: number; highest: number; lowest: number } {
-    if (numbers.length === 0) {
-      throw new Error("Array is empty");
-    }
-  
-    const { sum, highest, lowest } = numbers.reduce(
-      (accumulator, number) => {
-        return {
-          sum: accumulator.sum + number,
-          highest: Math.max(accumulator.highest, number),
-          lowest: Math.min(accumulator.lowest, number),
-        };
-      },
-      { sum: 0, highest: numbers[0], lowest: numbers[0] }
-    );
-  
-    const average = sum / numbers.length;
-  
-    return { average, highest, lowest };
+export function calculateAverageWithMinMax(numbers: number[]): {
+  average: number;
+  highest: number;
+  lowest: number;
+} {
+  if (numbers.length === 0) {
+    throw new Error("Array is empty");
   }
+
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  const average = sum / numbers.length;
+  const highest = Math.max(...numbers);
+  const lowest = Math.min(...numbers);
+
+  return { average, highest, lowest };
+}
